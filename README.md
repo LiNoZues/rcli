5e754568aabea2385b2b4086d2a80621c09d0f40
A_zz <1781311641@qq.com>
2024-05-27 22:24:15 +0800

    upport csv to yaml and json

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # RCLI
 
 <!-- # https://www.freecodecamp.org/chinese/news/how-to-open-visual-studio-code-from-your-terminal/
+学习项目地址: https://github.com/tyr-rust-bootcamp
 
-
-# git commit -a 报错解决
+# git commit -a denny 报错解决
 # https://github.com/EmbarkStudios/cargo-deny/issues/641
 # 1、clear the allow scope in deny.toml
 # 2、run cargo deny check, it will fail.
 # 3、fill licenses in the allow scope again.
 # 4、it works. -->
+
+
+# git commit -a fmt 报错解决
--- src/opts.rs
+++ src/opts.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use std::path::Path;
+use std::{fmt, path::Path, str::FromStr};
 #[derive(Debug, Parser)]
 #[command(name="rcli",version,author,about,long_about= None)]
 pub struct Opts {
@@ -11,13 +11,21 @@ pub enum Subcommand {
     #[command(name = "csv", about = "show csv or convert csv to other formats")]
     Csv(CsvOpts),
 }
+#[derive(Debug, Clone, Copy)]
+pub enum Outputformat {
+    Json,
+    Yaml,
+    // Toml,
+}
+
 #[derive(Debug, Parser)]
 pub struct CsvOpts {
     #[arg(short,long,value_parser = verify_input_file)]
     pub input: String,
-    #[arg(short, long, default_value = "output.json")]
-    // default_value 会自动转换数据类型来符合期望的数据类型  即 "output.json".into()
-    pub output: String,
+    #[arg(long)]
+    pub output: Option<String>,
+    #[arg(short,long,value_parser=parse_format, default_value = "json")]
+    pub output_format: Outputformat,
     #[arg(short, long, default_value_t = ',')]
     pub delimiter: char,
     #[arg(long, default_value_t = true)]
@@ -28,5 +36,37 @@ fn verify_input_file(filename: &str) -> Result<String, &'static str> {
         Ok(filename.into())
     } else {
         Err("File not found")
+    }
+}
+
+fn parse_format(fmt: &str) -> Result<Outputformat, anyhow::Error> {
+    fmt.parse()
+}
+
+impl From<Outputformat> for &'static str {
+    fn from(format: Outputformat) -> Self {
+        match format {
+            Outputformat::Json => "json",
+            Outputformat::Yaml => "yaml",
+            // Outputformat::Toml=>"toml",
+        }
+    }
+}
+
+impl FromStr for Outputformat {
+    type Err = anyhow::Error;
+    fn from_str(value: &str) -> Result<Self, Self::Err> {
+        match value.to_lowercase().as_str() {
+            "json" => Ok(Outputformat::Json),
+            "yaml" => Ok(Outputformat::Yaml),
+            // "toml"=> Ok(Outputformat::Toml),
+            v => anyhow::bail!("Unsupported format :{}", v),
+        }
+    }
+}
+
+impl fmt::Display for Outputformat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", Into::<&str>::into(*self))
     }
 }
